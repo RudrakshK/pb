b7d4ca1c62a8cfd1dacc634ed2bcf7cbccfab117
RudrakshK <stark.rudraksh@gmail.com>
2019-09-27 16:48:28 +0530

    Continued attempt

---
+++ backend/static/react/precache-manifest.9968ca9a4b258f33eb8bb1849596b968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5100f0bd8c2da37f205fae2d853b0b3d",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "9b73ffcfa008c773087e",
+    "url": "/static/react/css/main.4c381631.chunk.css"
+  },
+  {
+    "revision": "a69affd65eab1a72f70b",
+    "url": "/static/react/js/2.ce2e90bd.chunk.js"
+  },
+  {
+    "revision": "9b73ffcfa008c773087e",
+    "url": "/static/react/js/main.e6623990.chunk.js"
+  },
+  {
+    "revision": "43ed777154d10722679e",
+    "url": "/static/react/js/runtime-main.d4babbca.js"
+  }
+]);
